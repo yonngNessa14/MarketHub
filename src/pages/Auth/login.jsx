07d96c0c966a5ccb8bebc71557bc698c60e67cdd
yonngNessa14 <nessa4u1@gmail.com>
2021-06-11 00:21:40 +0100

    updated

--- src/pages/Auth/login.jsx
+++ src/pages/Auth/login.jsx
@@ -18,6 +18,15 @@ const Login = () => {
             <div>
               <AuthInput title="Courriel ou numéro de téléphone" placeholder="Entrer ici" />
               <AuthInput title="Mot de passe" placeholder="Tapez votre mot de passe" type="password" />
+              <div className="login--content__body-sublinks">
+                <div className="login--content__body-sublinks-1">
+                  <input type="checkbox" />
+                  <div className="login--content__body-sublinks-1_text">
+                    <p>Souviens-toi de moi</p>
+                  </div>
+                </div>
+                <h3>Mot de passe oublié?</h3>
+              </div>
             </div>
             <div>item 2</div>
           </div>
--- src/pages/Auth/loginCss.jsx
+++ src/pages/Auth/loginCss.jsx
@@ -33,6 +33,80 @@ export const LoginWrapper = style.div`
                 display: flex;
                 justify-content: space-between;
                 margin-top: 25px;
+
+                .login--content__body-sublinks {
+                    // border: 1px solid red;
+                    display: flex;
+                    align-items: center;
+                    justify-content: space-between;
+
+                    .login--content__body-sublinks-1 {
+                        display: flex;
+                        align-items: center;
+
+                        input {
+                            background: #E60000 !important;
+                            margin: 0;
+                            &:focus {
+                                background: #E60000 !important;
+                            }
+                        }
+
+                        input[type=checkbox] {
+                            -webkit-appearance: none;
+                            -moz-appearance: none;
+                            appearance: none;
+                            /* create custom checkbox appearance */
+                            display: inline-block;
+                            width: 20px;
+                            height: 20px;
+                            padding: 3px;
+                            /* background-color only for content */
+                            background-clip: content-box;
+                            // border: 1.5px solid #bbbbbb;
+                            border-radius: 3px;
+                            // background-color: #e7e6e7;
+                            margin-left: 15px;
+                            margin-right: 15px;
+                        
+                            &:checked{
+                                // background-color: #ff0000;
+                                color: white;
+                            }
+                        
+                            &:focus{
+                                outline: none !important;
+                            }
+                        }
+
+                        .login--content__body-sublinks-1_text {
+                            margin-left: 10px;
+                            display: flex;
+                            align-items: center;
+
+                            p {
+                                font-family: 'Manrope', sans-serif;
+                                font-weight: 400;
+                                font-size: 14px;
+                                // line-height: 165%;
+                                color: #353535;
+                                margin: 0;
+                            }
+                        }
+                    }
+                    
+                    h3 {
+                        color: #E60000;
+                        margin: 0;
+                        font-family: 'Manrope', sans-serif;
+                        font-weight: bold;
+                        font-size: 14px;
+                    }
+
+                    
+
+                    
+                }
             }
         }
     }
